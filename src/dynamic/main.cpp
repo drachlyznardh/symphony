f7d1554a5c137d5f0a4fb5e7eb72a2eed940709c
Ivan Simonini <ivan.simonini@roundhousecode.com>
2013-07-10 14:19:51 +0200

    Very simple parser invocation and result checking.

--- src/dynamic/main.cpp
+++ src/dynamic/main.cpp
@@ -2,7 +2,14 @@
 #include <cstdlib>
 #include <iostream>
 
+#include "DynamicParser.h"
+
 int main()
 {
+	tbd::DynamicParser parser;
+	
+	if (parser.parse() == 0) std::cout << "Yay" << std::endl;
+	else std::cout << "Nay" << std::endl;
+	
 	std::exit(0);
 }
